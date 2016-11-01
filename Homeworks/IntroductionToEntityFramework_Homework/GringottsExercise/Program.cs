f20842f6699ebb805a3c48d960549d7d7735cf8e
nelito987 <nelito987@abv.bg>
2016-11-01 15:49:31 +0200

    homework finished

---
+++ Homeworks/IntroductionToEntityFramework_Homework/GringottsExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GringottsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new GringottsContext();
+            var wizardsFirstNames = context.WizzardDeposits
+                                             .Where(d => d.DepositGroup == "Troll Chest")
+                                             .Select(d => d.FirstName.Substring(0, 1))
+                                             .Distinct()
+                                             .OrderBy(s => s);
+            foreach (var letter in wizardsFirstNames)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
--- Homeworks/IntroductionToEntityFramework_Homework/IntroductionToEntityFramework_Homework/Program.cs
+++ Homeworks/IntroductionToEntityFramework_Homework/IntroductionToEntityFramework_Homework/Program.cs
@@ -12,7 +12,7 @@ namespace IntroductionToEntityFramework_Homework
     {
         static void Main(string[] args)
         {
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-EN");
 
             var context = new SoftuniContext();
             // -- 3 and 4
@@ -98,20 +98,109 @@ namespace IntroductionToEntityFramework_Homework
             //Console.WriteLine(string.Join("\n", projects));
 
             // --- 8
-            var employeesProjects = context.Projects
-                .Where(p => p.StartDate.Year >= 2001 && p.StartDate.Year <= 2003)
-                .SelectMany(p => p.Employees)
-                .Distinct()
-                .Take(30);
+            //var employeesProjects = context.Projects
+            //    .Where(p => p.StartDate.Year >= 2001 && p.StartDate.Year <= 2003)
+            //    .SelectMany(p => p.Employees)
+            //    .Distinct()
+            //    .Take(30);
 
-            foreach (var emp in employeesProjects)
-            {
-                Console.WriteLine($"{emp.FirstName} {emp.LastName} {emp.Manager.FirstName}");
-                foreach (var proj in emp.Projects)
-                {
-                    Console.WriteLine($"--{proj.Name} {proj.StartDate} {proj.EndDate}");
-                }
-            }
+            //foreach (var emp in employeesProjects)
+            //{
+            //    Console.WriteLine($"{emp.FirstName} {emp.LastName} {emp.Manager.FirstName}");
+            //    foreach (var proj in emp.Projects)
+            //    {
+            //        Console.WriteLine($"--{proj.Name} {proj.StartDate} {proj.EndDate}");
+            //    }
+            //}
+
+            // --- 9
+
+            //var adresses = context.Addresses                
+            //    .OrderByDescending(a => a.Employees.Count())
+            //    .ThenBy(a => a.Town.Name)
+            //    .Take(10);
+
+            //foreach(var a in adresses)
+            //{
+            //    Console.WriteLine($"{a.AddressText}, {a.Town.Name} - {a.Employees.Count()} employees");
+            //}
+
+            // --- 10 -- to be checked in Profiler
+            //var employee147 = context.Employees
+            //    .Where(e => e.EmployeeID == 147)
+            //    .Select(e => new
+            //    {
+            //        e.FirstName,
+            //        e.LastName,
+            //        e.JobTitle,
+            //        e.Projects
+            //    });
+
+            //foreach(var e in employee147)
+            //{
+            //    Console.WriteLine($"{e.FirstName} {e.LastName} {e.JobTitle}");
+            //    var projects = e.Projects.OrderBy(p => p.Name);
+
+            //    foreach(var p in projects)
+            //    {
+            //        Console.WriteLine($"{p.Name}");
+            //    }
+            //}
+
+            // --- 11
+            //var departments = context.Departments
+            //    .Where(d => d.Employees.Count > 5)
+            //    .OrderBy(d => d.Employees.Count());
+
+            //foreach(var d in departments)
+            //{
+            //    Console.WriteLine($"{d.Name} {d.Employee.FirstName}");
+            //    foreach (var emp in d.Employees)
+            //    {
+            //        Console.WriteLine($"{emp.FirstName} {emp.LastName} {emp.JobTitle}");
+            //    }
+            //}
+
+            // --- 15
+            //var projects = context.Projects
+            //    .OrderByDescending(p => p.StartDate)
+            //    .Take(10)
+            //    .OrderBy(p => p.Name)
+            //    .Select(p => new {
+            //        p.Name,
+            //        p.Description,
+            //        p.StartDate,
+            //        p.EndDate
+            //    });
+
+            //foreach (var project in projects)
+            //{
+            //    Console.WriteLine($"{project.Name} {project.Description} {project.StartDate} {project.EndDate}");
+            //}
+
+            // --- 16
+            //var empToIncreaseSalary = context.Employees
+            //    .Where(e => e.Department.Name == "Engineering" ||
+            //                e.Department.Name == "Tool Design" ||
+            //                e.Department.Name == "Marketing" ||
+            //                e.Department.Name == "Information Services");
+
+            //foreach(var e in empToIncreaseSalary)
+            //{
+            //    e.Salary += e.Salary * (decimal)0.12;
+            //    Console.WriteLine($"{e.FirstName} {e.LastName} (${e.Salary})");
+            //}
+            //context.SaveChanges();
+
+            // --- 18
+            //var empStartingWith = context.Employees
+            //    .Where(e => e.FirstName.Substring(0, 2) == "Sa");
+
+            //foreach (var employee in empStartingWith)
+            //{
+            //    Console.WriteLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle} - (${employee.Salary})");
+            //}
+
         }
     }
 }
